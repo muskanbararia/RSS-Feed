dc90333a380c344eec123d90e3612afa8508da5e
Muskan <muskanbararia@gmail.com>
2019-04-24 00:45:08 +0530

    Added to heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/the-real-captain-marvel'));
+app.use(express.static(__dirname + '/dist/feed'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/the-real-captain-marvel/index.html'));
+res.sendFile(path.join(__dirname+'/dist/feed/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
